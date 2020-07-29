8a1753b563ccf5be3ca243dfda234b64d758334b
Yangyq1451 <985558237@qq.com>
2020-07-29 15:44:43 +0800

    complete stem

--- network.py
+++ network.py
@@ -4,25 +4,25 @@ import mindspore.ops.operations as operator
 class Stem:
     def __init__(self, in_channels):
         super(Stem).__init__()
-        self.conv2d_1a_3x3 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, stride=2)
-        self.conv2d_2a_3x3 = nn.Conv2d(32, 32, 3, stride=1)
-        self.conv2d_2b_3x3 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+        self.conv2d_1a_3x3 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, stride=2, pad_mode='valid')
+        self.conv2d_2a_3x3 = nn.Conv2d(32, 32, 3, stride=1, pad_mode='valid')
+        self.conv2d_2b_3x3 = nn.Conv2d(32, 64, 3, stride=1)
 
         self.mixed_3a_branch_0 = nn.MaxPool2d(3, stride=2, pad_mode='valid')
-        self.mixed_3a_branch_1 = nn.Conv2d(64, 96, 3, stride=2, padding=0)
+        self.mixed_3a_branch_1 = nn.Conv2d(64, 96, 3, stride=2, pad_mode='valid')
         self.cat_3a_branch = operator.Concat()
 
         self.mixed_4a_branch_0 = nn.SequentialCell([
             nn.Conv2d(160, 64, 1, stride=1),
-            nn.Conv2d(64, 96, 3, stride=1)
+            nn.Conv2d(64, 96, 3, stride=1, pad_mode='valid')
         ])
 
         self.mixed_4a_branch_1 = nn.Conv2d(160, 64, 1, stride=1)
         #### 这里需要拼接
-        self.layer1 = nn.Conv2d(64, 64, (1, 7), stride=1, pad_mode='same')
+        self.layer1 = nn.Conv2d(64, 64, (1, 7), stride=1)
         #### 这里需要拼接
         self.layer2 = nn.SequentialCell([
-            nn.Conv2d(64, 64, (7, 1), stride=1, pad_mode='same'),
+            nn.Conv2d(64, 64, (7, 1), stride=1),
             nn.Conv2d(64, 96, 3, stride=1, pad_mode='valid')
         ])
         self.cat_4a_branch = operator.Concat()
