e447724a3a1813ae410b5540cff20c40f0ca1b00
fengjk <1327246148@qq.com>
2020-08-17 10:20:39 +0800

    dataloader适配0.5版本

--- dataloader.py
+++ dataloader.py
@@ -1,33 +1,28 @@
-import numpy as np
-import mindspore.dataset.transforms.vision.c_transforms as C
-import mindspore.dataset as ds
-from mindspore.dataset.transforms.vision import Inter
-import mindspore.dataset.transforms.c_transforms as C2
-import mindspore.common.dtype as mstype
-from pathlib2 import Path
-import cv2 as cv
-
-
-# 辅助生产数据集的类，接口不用调用
-
-class Generator():
-
-    def __init__(self, image, label):
-        self.image = image
-        self.label = label
-
-    def __getitem__(self, item):
-        return (np.array(self.image[item]),
-                np.array(self.label[item]))  # Notice, tuple of only one element needs following a comma at the end.
-
-    def __len__(self):
-        return len(self.image)
-
-
-# 调用这个函数就行，传入两个list 分别为image，label （label采用1,2,3这种离散标注，而非01矩阵，返回的label也是离散标注）
-# 第三个参数是计算使用线程数，根据自己机器情况调整
+# Copyright 2020 Huawei Technologies Co., Ltd
 #
-def create_dataset(image, label, do_train, repeat_num=1, batch_size=4, num_parallel_workers=1, drop_remainder=False):
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+create train or eval dataset.
+"""
+import os
+import mindspore.common.dtype as mstype
+import mindspore.dataset as ds
+import mindspore.dataset.transforms.vision.c_transforms as C
+import mindspore.dataset.transforms.c_transforms as C2
+
+
+def create_dataset(dataset_path, do_train, repeat_num=1, batch_size=32, num_parallel_workers=1):
     """
     create a train or eval dataset
 
@@ -40,8 +35,15 @@ def create_dataset(image, label, do_train, repeat_num=1, batch_size=4, num_paral
     Returns:
         dataset
     """
+    # device_num = int(os.getenv("DEVICE_NUM"))
+    # # rank_id = int(os.getenv("RANK_ID"))
+    # rank_id = int(os.getenv("DEVICE_ID"))
 
-    dataset = ds.GeneratorDataset(source=Generator(image, label), column_names=["image", "label"], shuffle=True)
+    # if device_num == 1:
+    dataset = ds.ImageFolderDatasetV2(dataset_path, num_parallel_workers=num_parallel_workers, shuffle=True)
+    # else:
+    #     dataset = ds.ImageFolderDatasetV2(dataset_path, num_parallel_workers=num_parallel_workers, shuffle=True,
+    #                                       num_shards=device_num, shard_id=rank_id)
 
     image_size = 299
     mean = [0.485 * 255, 0.456 * 255, 0.406 * 255]
@@ -50,32 +52,27 @@ def create_dataset(image, label, do_train, repeat_num=1, batch_size=4, num_paral
     # define map operations
     if do_train:
         trans = [
-            # C.RandomCropDecodeResize(image_size, scale=(0.08, 1.0), ratio=(0.75, 1.333)),
-            # C.Decode(),
-            C.Resize((299, 299)),
+            C.RandomCropDecodeResize(image_size, scale=(0.08, 1.0), ratio=(0.75, 1.333)),
             C.RandomHorizontalFlip(prob=0.5),
             C.Normalize(mean=mean, std=std),
             C.HWC2CHW()
-            # C.Resize(image_size)
         ]
     else:
         trans = [
-            # C.Decode(),
+            C.Decode(),
             C.Resize((299, 299)),
-            # C.CenterCrop(image_size),
+            C.CenterCrop(image_size),
             C.Normalize(mean=mean, std=std),
             C.HWC2CHW()
         ]
 
     type_cast_op = C2.TypeCast(mstype.int32)
-    #
+
     dataset = dataset.map(input_columns=["image"], num_parallel_workers=num_parallel_workers, operations=trans)
     dataset = dataset.map(input_columns=["label"], num_parallel_workers=num_parallel_workers, operations=type_cast_op)
-    #
-    # # apply batch operations
-    buffer_size = 10000
-    dataset = dataset.shuffle(buffer_size=buffer_size)
-    dataset = dataset.batch(batch_size, drop_remainder=drop_remainder)
+
+    # apply batch operations
+    dataset = dataset.batch(batch_size, drop_remainder=True)
 
     # apply dataset repeat operation
     dataset = dataset.repeat(repeat_num)
@@ -83,38 +80,7 @@ def create_dataset(image, label, do_train, repeat_num=1, batch_size=4, num_paral
     return dataset
 
 
-# 测试用函数，，，不用管
-def get_image_iterator(dir, trans=False, size=None, begin=None):
-    images = []
-    name = []
-    for cnt, i in enumerate(Path.iterdir(dir)):
-        if i.is_file() and i.suffix == ".JPEG":
-            if begin and cnt < begin:
-                continue
-            src = cv.imread(str(i))
-            name.append(i.stem)
-            # src = cv.bilateralFilter(src, 10, 50, 50)  # 训练1
-            # src = cv.bilateralFilter(src, 7, 10, 10) #计算
-            # src = cv.bilateralFilter(src, 25, 50, 50)  # 训练2
-            # src = cv.bilateralFilter(src, 47, 100, 100)
-            # src = cv.pyrMeanShiftFiltering(src, 10, 50)
-
-            # src = cv.GaussianBlur(src, (5, 5), 100)
-            # src = cv.GaussianBlur(src, (11, 11), 2.5)
-            # src = np.array(cv.cvtColor(src, cv.COLOR_BGR2GRAY)).reshape((128, 128, 1))
-            if trans:
-                images.append(np.transpose(src, (2, 0, 1)))
-            else:
-                images.append(src)
-            if size:
-                if len(images) > size:
-                    break
-    return images, name
-
-
 if __name__ == "__main__":
-    image, image_name = get_image_iterator(Path("./ImageNet_train"))
-    dataset = create_dataset(image, range(len(image)), True)
-    # print(image)
-    for data in dataset.create_tuple_iterator():  # each data is a sequence
-        print(data)
+    dataset = create_dataset("./dataset", True, 2, 2)
+    for i in dataset.create_tuple_iterator():
+        print(i)
